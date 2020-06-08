ef00d922a7d82d165b169643d044d8eb4e6c93cc
Jeremy Crafts <jcrafts@redhat.com>
2020-06-08 16:34:27 -0400

    fix vars in data_collector
    Signed-off-by: Jeremy Crafts <jcrafts@redhat.com>

--- insights/client/data_collector.py
+++ insights/client/data_collector.py
@@ -90,7 +90,7 @@ class DataCollector(object):
         self.archive.add_metadata_to_archive(
             json.dumps(blacklist_report), '/blacklist_report')
 
-    def _write_rhsm_facts(self, hashed_hostname, ip_csv):
+    def _write_rhsm_facts(self, hashed_fqdn, ip_csv):
         logger.info('Writing RHSM facts...')
         ips_list = ''
         with open(ip_csv) as fil:
@@ -100,7 +100,7 @@ class DataCollector(object):
 
         facts = {
             'insights_client.obfuscate_hostname_enabled': self.config.obfuscate_hostname,
-            'insights_client.hostname': hashed_hostname,
+            'insights_client.hostname': hashed_fqdn,
             'insights_client.obfuscate_ip_enabled': self.config.obfuscate,
             'insights_client.ips': ips_list
         }
@@ -303,7 +303,7 @@ class DataCollector(object):
                 os.remove(clean_opts.keyword_file.name)
 
             # generate RHSM facts at this point
-            self._write_rhsm_facts(cleaner.hashed_hostname, cleaner.ip_report)
+            self._write_rhsm_facts(cleaner.hashed_fqdn, cleaner.ip_report)
 
             if self.config.output_dir:
                 # return the entire soscleaner dir
